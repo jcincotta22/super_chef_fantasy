6c9a155f76ff25f182c07b472041ea44601a3737
jcincotta22 <jcincotta22@gmail.com>
2017-04-14 18:06:31 -0400

    added modules

--- app.js
+++ app.js
@@ -2,68 +2,10 @@ const express = require('express');
 const keys = require('./keys.js')
 const app = express();
 const rp = require('request-promise')
-let response;
-let gameKey = 'mlb.l.17560'
-let url = "https://api.login.yahoo.com/oauth2/get_token"
-let options = {
-    "method": 'POST',
-    "uri": url,
-    "headers": {
-    'Authorization': `Basic ${keys.base64token}`,
-    'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    'body': `grant_type=authorization_code&redirect_uri=oob&code=${keys.code}`
-};
+const getStandings = require('./oauthRequests.js')
 
-let refresh = {
-      "method": 'POST',
-      "uri": "https://api.login.yahoo.com/oauth2/get_token",
-      "headers": {
-      'Authorization': `Basic ${keys.base64token}`,
-      'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      'body': `grant_type=refresh_token&redirect_uri=oob&refresh_token=${keys.refresh_token}`
-  };
-
-let refreshToken = (refresh) => {
-  return rp(refresh).then((data) => {
-    response = JSON.parse(data)
-  }).catch((err) => {
-    throw err
-  })
-}
-
-let getToken = (options) => {
-  return rp(options).then((data) => {
-    response = JSON.parse(data)
-  }).catch((err) => {
-    throw err
-  })
-}
-let newData;
-getToken(options).then((data) => {
-  console.log("access token", response.access_token)
+getStandings().then((data) => {
+  console.log(data)
 }).catch((err) => {
-  refreshToken(refresh).then((data) => {
-    console.log("access token after refresh", response.access_token)
-    console.log(response)
-    // needs to be refactored into a seperate method...but works.
-    let getLeagueUri = `https://fantasysports.yahooapis.com/fantasy/v2/league/mlb.l.17560/standings?format=json`
-    let getLeagueOptions = {
-      "method": 'GET',
-      "uri": getLeagueUri,
-      "headers": {
-        'Authorization': `Bearer ${response.access_token}`,
-        'Content-Type': 'application/json'
-      }
-    };
-    rp(getLeagueOptions).then((data) => {
-      newData = JSON.parse(data)
-      console.log(newData)
-    }).catch((err) => {
-      console.log(err)
-    })
-
-
-  })
-});
+  console.log(err)
+})
---
+++ oauthRequests.js
@@ -0,0 +1,89 @@
+const keys = require('./keys.js')
+const rp = require('request-promise')
+let response;
+let standings;
+let gameKey = 'mlb.l.17560'
+let url = "https://api.login.yahoo.com/oauth2/get_token"
+
+let refreshToken = (base64token, refreshToken) => {
+  let refresh = {
+        "method": 'POST',
+        "uri": url,
+        "headers": {
+        'Authorization': `Basic ${base64token}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        'body': `grant_type=refresh_token&redirect_uri=oob&refresh_token=${refreshToken}`
+    };
+  return rp(refresh).then((data) => {
+    response = JSON.parse(data)
+    console.log('saved new access token after refresh')
+  }).catch((err) => {
+    throw err
+  })
+}
+
+let getToken = (base64token, code) => {
+  let options = {
+      "method": 'POST',
+      "uri": url,
+      "headers": {
+      'Authorization': `Basic ${base64token}`,
+      'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      'body': `grant_type=authorization_code&redirect_uri=oob&code=${code}`
+  };
+  return rp(options).then((data) => {
+    response = JSON.parse(data)
+    console.log("token did not expire")
+  }).catch((err) => {
+    throw err
+  })
+}
+
+let getStandings = () => {
+  let getLeagueUri = `https://fantasysports.yahooapis.com/fantasy/v2/league/${gameKey}/standings?format=json`
+  return Promise.resolve()
+  .then(() => {
+    return getToken(keys.base64token, keys.code).then((data) => {
+      console.log("access token", response.access_token)
+    })
+  })
+  .then(() => {
+    let getLeagueOptions = {
+      "method": 'GET',
+      "uri": getLeagueUri,
+      "headers": {
+        'Authorization': `Bearer ${response.access_token}`
+      }
+    };
+    return rp(getLeagueOptions).then((data) => {
+      standings = JSON.parse(data)
+      return standings
+    }).catch((err) => {
+      throw err.message
+    });
+  })
+  .catch((err) => {
+    return Promise.resolve()
+    .then(() => {
+      return refreshToken(keys.base64token, keys.refresh_token)
+    })
+    .then(() => {
+      let getLeagueOptions = {
+        "method": 'GET',
+        "uri": getLeagueUri,
+        "headers": {
+          'Authorization': `Bearer ${response.access_token}`
+        }
+      };
+      return rp(getLeagueOptions).then((data) => {
+        standings = JSON.parse(data)
+        console.log(standings)
+        return standings
+      });
+    })
+  });
+}
+
+module.exports = getStandings;
